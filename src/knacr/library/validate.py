eda3cfb5392213b2f7b2a95b69f6aad34d7bea58
Artur Lissin <artur.lissin@dsmz.de>
2023-05-15 08:26:23 +0000

    feat: Check for missing 'changed to' ids

--- src/knacr/library/validate.py
+++ src/knacr/library/validate.py
@@ -10,17 +10,35 @@ from knacr.schemas.acr_db import ACR_DB
 _TJ = TypeVar("_TJ")
 
 
-def _validate_acr_db_dc(to_eval_acr: dict[int, AcrDb], /) -> None:
-    for ind in range(1, max(to_eval_acr.keys()) + 1):
-        if ind not in to_eval_acr:
+def _check_unique(
+    uniqueness: set[tuple[str, str, str]], acr_db: AcrDb, /
+) -> tuple[str, str, str]:
+    unique_id = (acr_db.code, acr_db.acr, acr_db.name)
+    if unique_id in uniqueness:
+        raise ValJsonEx(f"{unique_id} was seen more than once, but should be unique")
+    return unique_id
+
+
+def _check_changed_to_id(all_ids: set[int], acr_db: AcrDb, /) -> None:
+    for acr_cha in acr_db.acr_changed_to:
+        if acr_cha.id not in all_ids:
+            raise ValJsonEx(f"missing 'changed to' acr id {acr_cha.id}")
+
+
+def _check_missing_link_id(all_ids: set[int], /) -> None:
+    for ind in range(1, max(all_ids) + 1):
+        if ind not in all_ids:
             raise ValJsonEx(f"missing acr id {ind}")
-    uniqueness = set()
+
+
+def _validate_acr_db_dc(to_eval_acr: dict[int, AcrDb], /) -> None:
+    all_ids = set(to_eval_acr.keys())
+    _check_missing_link_id(all_ids)
+    uniqueness: set[tuple[str, str, str]] = set()
     for acr_db in to_eval_acr.values():
         check_uri_template(acr_db.catalogue)
-        unique_id = (acr_db.code, acr_db.acr, acr_db.name)
-        if unique_id in uniqueness:
-            raise ValJsonEx(f"{unique_id} was seen more than once, but should be unique")
-        uniqueness.add(unique_id)
+        uniqueness.add(_check_unique(uniqueness, acr_db))
+        _check_changed_to_id(all_ids, acr_db)
 
 
 def validate_acr_db_schema(to_eval: _TJ, /) -> None:
