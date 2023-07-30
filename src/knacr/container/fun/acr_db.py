eae322b06cccda76b4e26f3ee469ed4c4afe6fe0
Artur Lissin <artdotlis@pm.me>
2023-07-30 21:10:39 +0000

    feat: add core id separation

--- src/knacr/container/fun/acr_db.py
+++ src/knacr/container/fun/acr_db.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import asdict
 from re import Pattern
 import re
@@ -50,28 +51,64 @@ def create_acr_min_db(to_eval: dict[str, _TJ], /) -> dict[int, tuple[str, bool]]
 _CAT_ACR: Final[Pattern[str]] = re.compile(r"\{acr\}")
 _CAT_ID: Final[Pattern[str]] = re.compile(r"\{id\}")
 _CAT_PRE: Final[Pattern[str]] = re.compile(r"\{pre\}")
-_CAT_CORE: Final[Pattern[str]] = re.compile(r"\{core\}")
+_CAT_CORE: Final[Pattern[str]] = re.compile(r"\{core(:\d+)?\}")
 _CAT_SUF: Final[Pattern[str]] = re.compile(r"\{suf\}")
 
 _OPT_VAL: Final[Pattern[str]] = re.compile(r"({[^{]+})?<.+?>({.+?})?")
 
 _VALID_URI: Final[Pattern[str]] = re.compile(r"\{([^}]+?)\}")
 _VALID_PAR: Final[set[str]] = {"acr", "id", "pre", "core", "suf"}
+_VALID_PAR_SUB: Final[set[str]] = {"core"}
+_ID_SUB: Final[Pattern[str]] = re.compile("^(.+?)(:\d+)?$")
+_ID_SEP: Final[Pattern[str]] = re.compile(r"[^A-Za-z0-9]")
 
 
 def check_uri_template(uri: str, /) -> None:
+    sub_parts = defaultdict(list)
     for param in _VALID_URI.findall(uri):
-        if param not in _VALID_PAR:
+        arg_parts = _ID_SUB.match(param)
+        if arg_parts is None or arg_parts.group(1) not in _VALID_PAR:
             raise ValJsonEx(f"invalid param name detected [{param}]")
+        if arg_parts.group(2) is not None:
+            sub_parts[arg_parts.group(1)].append(int(arg_parts.group(2).strip(":")))
+            if arg_parts.group(1) not in _VALID_PAR_SUB:
+                raise ValJsonEx(
+                    f"only {_VALID_PAR_SUB!s} are allowed to be defined as arrays"
+                )
+    for param, arr_id in sub_parts.items():
+        if 0 in arr_id:
+            raise ValJsonEx(f"zeros are not allowed as index for params [{param}]")
 
 
-_REPL_PARAM: Final[dict[str, Callable[[CatArgs], str]]] = {
-    "{acr}": lambda cat: cat.acr,
-    "{id}": lambda cat: cat.id,
-    "{pre}": lambda cat: cat.pre,
-    "{core}": lambda cat: cat.core,
-    "{suf}": lambda cat: cat.suf,
+def _parse_id_core(mat: str, id_core: str, /) -> dict[str, str]:
+    ids = _ID_SEP.split(id_core)
+    num = _CAT_CORE.match(mat)
+    if num is None:
+        raise ValJsonEx(f"mismatched core id tag: {mat} -> {id_core}")
+    if num.group(1) == "":
+        return {mat: id_core}
+    id_num = int(num.group(1).strip(":"))
+    if id_num > len(ids):
+        return {mat: ""}
+    return {mat: ids[id_num - 1]}
+
+
+_REPL_PARAM: Final[dict[Pattern[str], Callable[[str, CatArgs], dict[str, str]]]] = {
+    _CAT_ACR: lambda mat, cat: {mat: cat.acr},
+    _CAT_ID: lambda mat, cat: {mat: cat.id},
+    _CAT_PRE: lambda mat, cat: {mat: cat.pre},
+    _CAT_CORE: lambda mat, cat: _parse_id_core(mat, cat.core),
+    _CAT_SUF: lambda mat, cat: {mat: cat.suf},
 }
+
+
+def _get_repl_param_fun(
+    href_part: str, /
+) -> tuple[str, Callable[[str, CatArgs], dict[str, str]]] | None:
+    for che, repl in _REPL_PARAM.items():
+        if (mat := che.search(href_part)) is not None:
+            return mat.group(0), repl
+    return None
 
 
 def _rm_opt(href: str, left: str, right: str, /) -> str:
@@ -80,15 +117,29 @@ def _rm_opt(href: str, left: str, right: str, /) -> str:
     )
 
 
+def _get_left_right(
+    left: str, right: str, args: CatArgs, /
+) -> tuple[dict[str, str], dict[str, str]]:
+    left_f, right_f = _get_repl_param_fun(left), _get_repl_param_fun(right)
+    if left_f is None or right_f is None:
+        raise ValJsonEx(f"uri contains unknown parameter names: {left} - {right}")
+    return left_f[1](left_f[0], args), right_f[1](right_f[0], args)
+
+
+def _are_params_empty(param_con: dict[str, str], /) -> bool:
+    for p_val in param_con.values():
+        if p_val != "":
+            return False
+    return True
+
+
 def _fix_opt(href: str, match: tuple[str, str], args: CatArgs, /) -> str:
     left, right = match
     if "" in [left, right]:
         return _rm_opt(href, left, right)
-    left_f, right_f = _REPL_PARAM.get(left, None), _REPL_PARAM.get(right, None)
-    if left_f is None or right_f is None:
-        raise ValJsonEx(f"uri contains unknown parameter names - {href}")
-    left_p, right_p = left_f(args), right_f(args)
-    if "" in [left_p, right_p]:
+    left_p, right_p = _get_left_right(left, right, args)
+    print(left_p, right_p)
+    if _are_params_empty(left_p) or _are_params_empty(right_p):
         return _rm_opt(href, left, right)
     return re.compile(re.escape(left) + r"<(.+?)>" + re.escape(right)).sub(
         f"{left}\g<1>{right}", href, 1
@@ -98,8 +149,8 @@ def _fix_opt(href: str, match: tuple[str, str], args: CatArgs, /) -> str:
 def replace_param_value(href: str, args: CatArgs, /) -> str:
     for opt in _OPT_VAL.findall(href):
         href = _fix_opt(href, opt, args)
-    href = _CAT_ACR.sub(args.acr, href)
-    href = _CAT_ID.sub(args.id, href)
-    href = _CAT_PRE.sub(args.pre, href)
-    href = _CAT_CORE.sub(args.core, href)
-    return _CAT_SUF.sub(args.suf, href)
+    for che, repl in _REPL_PARAM.items():
+        for mat in che.finditer(href):
+            for to_repl, repl_val in repl(mat.group(0), args).items():
+                href = href.replace(to_repl, repl_val)
+    return href
