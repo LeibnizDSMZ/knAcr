50c47f3172e4c0d1160e42170bf89e8f6c5210d7
Artur Lissin <artur.lissin@dsmz.de>
2023-10-19 13:07:33 +0000

    feat: add function to fill core ids with leading zeroes

--- tests/fixture/links.py
+++ tests/fixture/links.py
@@ -16,3 +16,13 @@ def ccno_iam_1() -> tuple[int, CatArgs]:
 @pytest.fixture()
 def ccno_nrrl_1() -> tuple[int, CatArgs]:
     return 17, CatArgs(acr="NRRL", id="B-1", pre="B", suf="", core="1")
+
+
+@pytest.fixture()
+def ccno_dcg_1() -> tuple[int, CatArgs]:
+    return 82, CatArgs(acr="DCG", id="1", pre="", suf="", core="1")
+
+
+@pytest.fixture()
+def ccno_lmg_1_1() -> tuple[int, CatArgs]:
+    return 2, CatArgs(acr="LMG", id="1.1", pre="", suf="", core="1.1")
