d776821e0f577b818660903352056f9aa2959456
Artur Lissin <artur.lissin@dsmz.de>
2023-11-27 14:45:34 +0000

    feat: add catalogue example db

--- tests/library/test_loader.py
+++ tests/library/test_loader.py
@@ -8,6 +8,7 @@ from knacr.library.loader import (
     load_acr_db,
     load_min_acr_db,
     load_regex_db,
+    load_catalogue_db,
     parse_acr_db,
 )
 
@@ -84,5 +85,27 @@ class TestLoader:
         acr_db = parse_acr_db(json.loads(load_fix_acr_db))
         try:
             load_regex_db(acr_db, STABLE_VER)
+        except (ValJsonEx, ReqURIEx) as val_ex:
+            pytest.fail(f"regex data malformed - {val_ex.message}")
+
+    @patch("knacr.library.loader.requests")
+    def test_load_catalogue_db_fail(self, req: MagicMock) -> None:
+        resp = MagicMock()
+        resp.ok = False
+        req.get.return_value = resp
+        with pytest.raises(ReqURIEx):
+            load_catalogue_db({}, "never_tag")
+
+    @patch("knacr.library.loader.requests")
+    def test_load_catalogue_db_success(
+        self, req: MagicMock, load_fix_catalogue_db: bytes, load_fix_acr_db: bytes
+    ) -> None:
+        resp = MagicMock()
+        resp.ok = True
+        resp.json.return_value = json.loads(load_fix_catalogue_db)
+        req.get.return_value = resp
+        acr_db = parse_acr_db(json.loads(load_fix_acr_db))
+        try:
+            load_catalogue_db(acr_db, STABLE_VER)
         except (ValJsonEx, ReqURIEx) as val_ex:
             pytest.fail(f"regex data malformed - {val_ex.message}")
